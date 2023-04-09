6591da4b53b542668ac9f1576788fb0e7c3c6ccd
Iced-code <86176543+Iced-code@users.noreply.github.com>
2023-04-08 23:57:11 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int main(void)
+{
+	BOOL bFile;
+	bFile = MoveFile(
+		L"C:\\temp\\test1.txt",
+		L"C:\\temp\\test3.txt"
+	);
+	if (bFile == FALSE)
+	{
+		printf("MoveFile failed and error no %ul\n", GetLastError());
+	}
+	else
+	{
+		printf("MoveFile Success!");
+	}
+}
